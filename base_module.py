6488f9e31da60becf419723c0e052b8be891ad46
gammacam <gammacam@gammacam.localdomain>
2017-05-03 10:15:40 -0500

    Updates to base_module and degauss-and-measure.  I could not tell you what the changes were.  Just committing for safe keeping.

--- base_module.py
+++ base_module.py
@@ -10,7 +10,7 @@ datetime = time.strftime('%d_%m_%Y__%H_%M_%S')
 
 voltagescale = "200E-2"
 voltagescalech3 = "200E-3"
-filename = datetime+"test"
+#filename = datetime+"test"
 
 class Instrument:
 
@@ -58,7 +58,7 @@ class degaussing(Instrument):
         #self.write("MMEMO:LOAD:DATA1 INT:\Builtin\DG.arb")
        # self.write("FUNC:ARB"INT:\Builtin\DG.arb"")
 
-    def func_gen_degauss(self, freq_degauss,volt_degauss,sample_rate):
+    def func_gen_degauss(self, freq_degauss,volt_degauss,offset_degauss,sample_rate):
         self.write("OUTP2 OFF")
         print('Agilent configured for degaussing sequence')
         self.write("SOUR1:BURS:STAT OFF") # Burst off
@@ -75,7 +75,8 @@ class degaussing(Instrument):
         # waveform in
         # order to
         # change ramp
-        self.write("SOUR1:VOLT:OFFS 0.00") # Carrier freq is 60 Hz @ 10 Vpp
+        offset_degauss_str = str(offset_degauss)
+        self.write("SOUR1:VOLT:OFFS "+offset_degauss_str)
         self.write("SOUR1:AM:DEPT 100") # Modulation depth = 100%
         self.write("SOUR1:AM:STAT ON") #Modulation on
         self.write("OUTP ON") # Turn on the instrument output
@@ -84,7 +85,7 @@ class degaussing(Instrument):
         # The waveform DG has 10^6 samples
         num_samples=1000000
         full_time=num_samples/sample_rate
-        wait_time=full_time*0.55
+        wait_time=full_time*0.5+2
         
         time.sleep(wait_time)
 #        self.write("OUTP:TRIG OFF") # Turn off
@@ -92,7 +93,7 @@ class degaussing(Instrument):
 #        self.write("SOUR1:VOLT 0.01")
 #        self.write("SOUR1:AM:DEPT 0") # Modulation depth = 0%
 #        self.write("SOUR1:AM:STAT OFF") # Turn AM modulation off
-        self.write("OUTP OFF ")
+        self.write("OUTP OFF")
         
         # Do some commands to stop triggers from coming (set up burst mode)
          #self.write("OUTP:TRIG:SOUR CH1")
@@ -116,3 +117,141 @@ class degaussing(Instrument):
         self.write("SOUR2:BURS:STAT ON")
         self.write("OUTP2 ON")
         self.write("*TRG")
+
+
+
+class TekScope(Instrument):
+    
+    """ Tektronix DPO 4054 class"""
+
+    def scope_degauss(self,mod_freq):
+        print("Scope configured for degaussing sequence")
+        self.write("TRIG:A:MOD AUTO")
+        self.write("ACQ:STATE RUN")
+        self.write("HOR:DEL:MOD OFF")
+	time_scale_degauss = 0.04/mod_freq
+	time_scale_degauss_str = str(time_scale_degauss)
+        self.write("HOR:SCA "+time_scale_degauss_str) # Has to be 0.04/mod_freq to get full degausse sequence
+        self.write("HOR:RECO 10000")
+        self.write("HOR:POS 0")
+        self.write("TRIG:A:EDGE:SOU LINE")
+	self.write("CH1:SCALe "+voltagescale) 
+        self.write("CH2:SCALe "+voltagescale)
+	self.write("CH3:SCALe "+voltagescalech3)
+	self.write("CH4:SCALe "+voltagescalech3)
+        
+    def scope_bh(self,freq_bh):
+        print("Scope configured for a B-H curve measurement")
+        self.write("TRIG:A:MOD NORMAL")
+        self.write("ACQ:STATE STOP")
+	time_scale_bh = 0.2/freq_bh
+	if time_scale_bh < 0.04:
+	    time_scale_bh = 0.04
+	#if time_scale_bh == 0.04:
+	    #time_scale_bh = 0.1
+        else :
+            time_scale_bh = time_scale_bh
+	time_scale_bh_str = str(time_scale_bh)
+        self.write("HOR:SCA "+time_scale_bh_str)
+        self.write("HOR:RECO 10000")
+        self.write("HOR:POS 0")
+        self.write("TRIG:A:EDGE:SOU CH4")
+        self.write("ACQ:STATE RUN")
+	self.write("CH1:SCALe "+voltagescale) 
+        self.write("CH2:SCALe "+voltagescale)
+	self.write("CH3:SCALe "+voltagescalech3)
+        time.sleep(1)
+
+####################### Data Acquisiton #######################
+    
+    def read_data( self ):
+        self.write( "CURV?" )
+        rawdata = self.read(20000) # 20000 saves whole scope 
+        return numpy.frombuffer( rawdata[7:-1], 'i2' )
+    
+    def scale_data( self,source ):
+
+        """method to scale data """
+
+        self.write("DATA:SOURCE " + source)
+        data = self.read_data()
+        print("Got data for "+ source)
+        self.write("WFMPRE:YMUlt?")
+        ymult = float(self.read(200))
+        self.write("WFMPRE:YOFF?")
+        yoff=float(self.read(200))
+        self.write("WFMPRE:YZERO?")
+        yzero = float(self.read(200))
+        print("For "+source,ymult,yoff,yzero)
+
+        data = ((data - yoff) * ymult) + yzero
+        return data
+
+    def get_xdata(self):
+
+        """Method to get horizontal data array from a scope"""
+
+        self.write("HORIZONTAL:MAIN:SCALE?")
+        self.timescale = float(self.read(20))
+        self.write("HORIZONTAL:MAIN:POSITION?")
+        self.timeoffset = float(self.read(20))
+        self.write("HORIZONTAL:RECORDLENGTH?")
+        self.time_size=int(self.read(30))
+
+        self.time=numpy.arange(0,self.timescale*10,self.timescale*10/self.time_size)
+        return self.time
+
+    """ reading scope"""
+
+    def adjusting(self):
+        self.write("ACQ:STATE STOP") # Stop data acquisition
+        self.write("DATA:WIDTH 2") # Set data width to 2 for better resolution
+        self.write("DATA:ENCD SRI") # Set data format to binary, zero is center-frame and big endian
+
+    def data_array(self):
+         
+        self.write("SELECT?")
+        self.wfms=self.read(20)
+        print(self.wfms)
+
+        # parse into array of characters
+        self.wfms = self.wfms.strip().split(";")
+
+        if self.wfms[0]=="1":
+            self.ch1data = self.scale_data("CH1")
+
+        if self.wfms[1]=="1":
+            self.ch2data = self.scale_data("CH2")
+
+        if self.wfms[2]=="1":
+            self.ch3data = self.scale_data("CH3")
+	if self.wfms[3]=="1":
+            self.ch4data = self.scale_data("CH4")
+
+    def time_scale(self):
+         #get the time scale
+
+        self.write("HORIZONTAL:MAIN:SCALE?")
+        self.timescale = float(self.read(20))
+
+        # Get the timescale offset
+        self.write("HORIZONTAL:MAIN:POSITION?")
+        self.timeoffset = float(self.read(20))
+
+        # Get the length of the horizontal record
+        self.write("HORIZONTAL:RECORDLENGTH?")
+        self.time_size = int(self.read(30))
+
+        self.time = numpy.arange(0,self.timescale*10,self.timescale*10/self.time_size)
+        print(self.time)
+#        self.write("ACQ:STATE RUN")
+
+    def save_data(self,runno):
+        filename='run'+str(runno).zfill(5)+'.dat'
+        print(filename)
+        with open(filename,'w') as thefile:
+            for item1,item2,item3,item4,item5 in zip(self.time,self.ch1data,self.ch2data,self.ch3data,self.ch4data):
+                print>>thefile,item1,item2,item3,item4,item5
+
+    def run(self):
+        self.write("ACQ:STATE RUN")
--- degauss-and-measure.py
+++ degauss-and-measure.py
@@ -2,6 +2,7 @@
 # Degaussing Program
 # Mon Jun 27 2016 Jeff and Henri
 # Fri Feb 17 15:07:39 CST 2017 Jeff and Mike removed the scope for degaussing.
+# Fri Mar 17 09:08:05 CDT 2017 recommend to "upgrade" to pyusb at some point.
 
 
 import base_module as bm
@@ -10,20 +11,47 @@ import time
 
 print('\nInitializing devices:\n')
 
-degaussing = bm.degaussing("/dev/usbtmc1")
+datetime = time.strftime('%d_%m_%Y__%H_%M_%S')
+
+degaussing = bm.degaussing("/dev/usbtmc0")
+scope_data = bm.TekScope("/dev/usbtmc1")
+
+runlog = "runlog.txt"
+runno=0
+try:
+    f = open(runlog,'r')
+except IOError:
+    print 'cannot open',runlog
+    runno=1
+else:
+    linelist = f.readlines()
+    f.close()
+    oldrunno=int(linelist[-1].split(' ')[0])
+    runno=oldrunno+1
+    print 'Last run was:  ',oldrunno
+    print 'This run is:  ',runno
+f = open(runlog,'a')
+
 
 print('I think the function generator is\n')
 degaussing.printName()
+print('I think the scope is\n')
+scope_data.printName()
+
 
 # Degauss Settings
 
 freq_degauss = 10
 volt_degauss = 10
-sample_rate = 100000
+offset_degauss = 0.0
+sample_rate = 20000
 
 #magnetic field parameters
 field_amp = .5
-field_freq= .1
+field_freq= .01
+
+f.write("%d %s %f %f %f %f %f %f\n" % (runno,datetime,freq_degauss,volt_degauss,offset_degauss,sample_rate,field_amp,field_freq))
+
 
 
 # if (raw_input("\nHave you set the ramp (Y)/N ?  ")=="N"):
@@ -32,13 +60,28 @@ field_freq= .1
 #print ('setting the ramp')
 #degaussing.rampset()
 print ('Please close the switch.')
+scope_data.run()
 raw_input("press ENTER to continue.")
 print('\nStarting degaussing')
 #    scope_data.scope_degauss(mod_freq)
-degaussing.func_gen_degauss(freq_degauss,volt_degauss,sample_rate)
+degaussing.func_gen_degauss(freq_degauss,volt_degauss,offset_degauss,sample_rate)
 print("Degaussing completed")
 
 print ('Please open the switch.')
-raw_input("pres ENTER to continue.")
+raw_input("press ENTER to continue.")
 
 degaussing.mag_field(field_amp,field_freq)
+
+time.sleep(1/field_freq+10)
+
+print("\nSaving data:")
+scope_data.adjusting()
+#raw_input("pres ENTER to continue.")
+
+scope_data.data_array()
+#raw_input("pres ENTER to continue.")
+
+scope_data.time_scale()
+#raw_input("pres ENTER to continue.")
+
+scope_data.save_data(runno)
